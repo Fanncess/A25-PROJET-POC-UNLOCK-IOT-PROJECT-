62a77833d25313f1d7f3105959bb668e99ce116d
2364669@cegepba.qc.ca <123336005+annema01@users.noreply.github.com>
2025-11-29 20:53:46 -0500

    config MQTT

---
+++ deverrouillage.py
@@ -0,0 +1,31 @@
+#import RPi.GPIO as GPIO
+import time
+from MQTT.mqtt_subscriber import Mqtt_Subscriber
+
+class Deverrouillage:
+    def __init__(self):
+        self.mqtt = Mqtt_Subscriber("localhost", 1883, handler=self)
+        #CONFIGURER LE RELAIS
+        self.RELAY_PIN = 17
+        self.UNLOCK_TIME = 3
+        #GPIO.setmode(GPIO.BCM)
+        #GPIO.setup(self.RELAY_PIN, GPIO.OUT)
+        self.Verrouiller()
+
+    def reception_mqtt(self, message):
+        if message == "1":
+            self.Deverrouiller()
+        elif message == "0":
+            self.Verrouiller()
+
+
+    def Verrouiller(self):    
+        #GPIO.output(self.RELAY_PIN, GPIO.LOW)
+        print("LOCK")
+
+    def Deverrouiller(self):    
+        #GPIO.output(self.RELAY_PIN, GPIO.HIGH)
+        print("UNLOCK for %u seconds" %self.UNLOCK_TIME)
+        time.sleep(self.UNLOCK_TIME)
+        self.Verrouiller()
+
